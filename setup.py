1ac147a2a9f627cccd917006f61cdda7b25ccc06
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-05 21:05:09 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='applied-sims',
+    version='0.1',
+    classifiers=[
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Other Audience',
+    ],
+    packages=['polymer_states'],
+    url='http://github.com/szabba/applied-sims',
+    license='MPL-2.0',
+    author='Karol Marcjan',
+    author_email='karol.marcjan@gmail.com',
+    description=''
+)
