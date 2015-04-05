534e2542fe009216b8c61789c68c1b5831da1203
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-05 21:27:42 +0200

    Add Polymer class

--- polymer_states/__init__.py
+++ polymer_states/__init__.py
@@ -2,7 +2,17 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import numpy
+
+
 # Link states
 UP, DOWN = (0, 1), (0, -1)
 LEFT, RIGHT = (-1, 0), (1, 0)
 SLACK = (0, 0)
+
+
+class Polymer:
+    """A chain of N links."""
+
+    def __init__(self, links):
+        self.__links = numpy.array(links)
