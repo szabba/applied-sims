b5b17c5152e969ed4e629a5df8dd296cde164f9b
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-05 21:27:32 +0200

    Add link states to polymer_states

--- polymer_states/__init__.py
+++ polymer_states/__init__.py
@@ -1,3 +1,8 @@
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Link states
+UP, DOWN = (0, 1), (0, -1)
+LEFT, RIGHT = (-1, 0), (1, 0)
+SLACK = (0, 0)
