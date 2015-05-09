848e5b499f712beef45b076df7225606c70c5775
Karol Marcjan <karol.marcjan@gmail.com>
2015-05-09 11:42:46 +0200

    Fix Polymer equality

--- polymer_states/__init__.py
+++ polymer_states/__init__.py
@@ -88,6 +88,7 @@ class Polymer:
         return hash(self.__links)
 
     def __eq__(self, other):
+        if not isinstance(other, Polymer): return False
         return self.__links == other._Polymer__links
 
     def __ne__(self, other):
